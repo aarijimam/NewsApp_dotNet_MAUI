878e4acc1611e3cdcc5a013d6b63930c5e52a016
aarijimam <35100854+aarijimam@users.noreply.github.com>
2024-04-01 10:13:32 +0500

    Update APIData.cs

--- NewsApp/Services/APIData.cs
+++ NewsApp/Services/APIData.cs
@@ -1,5 +1,6 @@
 ﻿using NewsApp.MVVM.Models;
 using Newtonsoft.Json;
+using System.Net.Http.Json;
 
 namespace NewsApp.Services
 {
@@ -9,19 +10,22 @@ namespace NewsApp.Services
 		{
 
             var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://newsapi.org/v2/top-headlines");
 
             if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet) return null;
 
             if (newsTopic == "all")
 			{
-                var jsonData = await httpClient.GetStringAsync("https://newsapi.org/v2/top-headlines?country=us&apiKey=7f8e7406839542c58100a71392a34567" + "&q=" + searchQuery);
+                var jsonData = await httpClient.GetStringAsync("current?country=us&apiKey=7f8e7406839542c58100a71392a34567" + "&q=" + searchQuery);
                 var result = JsonConvert.DeserializeObject<Root>(jsonData);
                 return result;
             }
 			else {
-				var jsonData = await httpClient.GetStringAsync("https://newsapi.org/v2/top-headlines?country=us&apiKey=7f8e7406839542c58100a71392a34567&category=" + newsTopic + "&q=" + searchQuery);
-                var result = JsonConvert.DeserializeObject<Root>(jsonData);
-                return result;
+                var address = $"https://newsapi.org/v2/top-headlines?country=us&apiKey=7f8e7406839542c58100a71392a34567&category={newsTopic}&q={searchQuery}";
+                Console.WriteLine(address);
+                var jsonData = await httpClient.GetFromJsonAsync<Root>($"https://newsapi.org/v2/top-headlines?country=us&apiKey=7f8e7406839542c58100a71392a34567&category={newsTopic}&q={searchQuery}");
+                // var result = JsonConvert.DeserializeObject<Root>(jsonData);
+                return jsonData;
             }
 
             
